11eb988683dd7d11e588fb9185f327e78319b591
Maximilian Köhl <mail@koehlma.de>
2023-11-17 13:18:21 +0100

    fix: fix recipes for Bookworm-based releases

--- recipes/pi-cleanup/steps/00-install.sh
+++ recipes/pi-cleanup/steps/00-install.sh
@@ -5,8 +5,13 @@ set -euo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 systemctl disable userconfig
-systemctl disable unattended-upgrades.service
 systemctl disable resize2fs_once
+
+# Only exists on older (Debuan Buster) releases.
+systemctl disable unattended-upgrades.service || true
+
+# Only exists on newer (Debian Bookworm) releases.
+systemctl disable sshswitch.service || true
 
 apt-get purge -y userconf-pi
 rm -f /etc/ssh/sshd_config.d/rename_user.conf
