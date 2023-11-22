704aff62dc7d3d5f5f9c59ecb558196da4118017
Maximilian Köhl <mail@koehlma.de>
2023-11-22 07:50:08 +0100

    chore: fix comments

--- boot/uboot/boot.sh
+++ boot/uboot/boot.sh
@@ -52,10 +52,10 @@ echo Bootargs: ${bootargs}
 load mmc 0:${bootpart} ${kernel_addr_r} ${kernel_file}
 setenv kernel_comp_addr_r ${loadaddr}
 setenv kernel_comp_size 0x4000000
-# Try booting `zImage`.
+# Try booting compressed kernel image.
 booti ${kernel_addr_r} - ${fdt_addr}
-# Try booting `uImage`.
-bootm ${kernel_addr_r} - ${fdt_addr}
+# Try booting `zImage` kernel.
+bootz ${kernel_addr_r} - ${fdt_addr}
 
 echo "Error loading kernel... Rebooting in 10 seconds."
 sleep 10
