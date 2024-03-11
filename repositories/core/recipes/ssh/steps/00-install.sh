6d2d917f9ef1a9c153ebc2599beaf680183092f8
Maximilian Köhl <mail@koehlma.de>
2024-03-11 09:36:48 +0100

    chore: do not fail when `regenerate_ssh_host_keys` service does not exist

--- repositories/core/recipes/ssh/steps/00-install.sh
+++ repositories/core/recipes/ssh/steps/00-install.sh
@@ -7,7 +7,7 @@ systemctl enable ssh
 install -D -m 744 "${RECIPE_DIR}/files/hydrate-ssh-host-keys.sh" -t /usr/lib/rugpi/scripts/
 install -D -m 644 "${RECIPE_DIR}/files/hydrate-ssh-host-keys.service" -t /usr/lib/systemd/system/
 
-systemctl disable regenerate_ssh_host_keys
+systemctl disable regenerate_ssh_host_keys || true
 systemctl enable hydrate-ssh-host-keys
 
 if [ "${RECIPE_PARAM_ROOT_AUTHORIZED_KEYS}" != "" ]; then
