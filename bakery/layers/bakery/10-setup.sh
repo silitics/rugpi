4c7464be17300a3dec571cc8baddedd8cb3e3a78
Maximilian Köhl <mail@koehlma.de>
2024-05-12 10:04:24 +0200

    chore: add Grub and Systemd Boot to Bakery container

--- bakery/layers/bakery/10-setup.sh
+++ bakery/layers/bakery/10-setup.sh
@@ -2,5 +2,26 @@
 
 set -euo pipefail
 
+# Install Grub for all the different architectures.
+dpkg --add-architecture amd64
+dpkg --add-architecture arm64
+dpkg --add-architecture armhf
+
+apt-get update -y
+
+apt-get install -y \
+    grub-common \
+    grub-efi-amd64-bin \
+    grub-efi-arm-bin \
+    grub-efi-arm64-bin \
+    grub-efi-ia32-bin \
+    grub-pc-bin \
+    systemd-boot-efi:amd64 \
+    systemd-boot-efi:arm64 \
+    systemd-boot-efi:armhf
+
+apt-get -y clean && rm -rf /var/lib/apt/lists/*
+
+
 cd /usr/share/rugpi/boot/u-boot
 ./verify.sh
