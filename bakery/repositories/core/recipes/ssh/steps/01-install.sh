f4c515d0f92de182e91ecc1b05d6b99e3c0bf2b7
Maximilian Köhl <mail@koehlma.de>
2024-06-05 22:26:59 +0200

    feat: adapt ssh recipe for Alpine Linux

--- bakery/repositories/core/recipes/ssh/steps/01-install.sh
+++ bakery/repositories/core/recipes/ssh/steps/01-install.sh
@@ -2,13 +2,23 @@
 
 set -euo pipefail
 
-systemctl enable ssh
-
 install -D -m 744 "${RECIPE_DIR}/files/hydrate-ssh-host-keys.sh" -t /usr/lib/rugpi/scripts/
-install -D -m 644 "${RECIPE_DIR}/files/hydrate-ssh-host-keys.service" -t /usr/lib/systemd/system/
 
-systemctl disable regenerate_ssh_host_keys || true
-systemctl enable hydrate-ssh-host-keys
+if command -v systemctl; then
+    systemctl enable ssh
+
+    install -D -m 644 "${RECIPE_DIR}/files/systemd/hydrate-ssh-host-keys.service" -t /usr/lib/systemd/system/
+
+    systemctl disable regenerate_ssh_host_keys || true
+    systemctl enable hydrate-ssh-host-keys
+fi
+
+if command -v rc-update; then
+    install -D -m 744 "${RECIPE_DIR}/files/openrc/hydrate-ssh-host-keys" -t /etc/init.d/
+
+    rc-update add sshd
+    rc-update add hydrate-ssh-host-keys
+fi
 
 if [ "${RECIPE_PARAM_ROOT_AUTHORIZED_KEYS}" != "" ]; then
     mkdir -p /root/.ssh
