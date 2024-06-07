7dc377c1bfe902bbc4a1aad803a215d160677de1
Maximilian Köhl <mail@koehlma.de>
2024-06-07 17:26:43 +0200

    fix: copy boot files correctly for Raspberry Pi

--- bakery/repositories/core/recipes/rpi-alpine-setup/steps/00-install.sh
+++ bakery/repositories/core/recipes/rpi-alpine-setup/steps/00-install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-BOOT_DIR="${RUGPI_BUNDLE_DIR}/roots/boot"
+BOOT_DIR="${RUGPI_BUNDLE_DIR}/roots/boot/"
 
 apk add linux-rpi
 
+mkdir -p "${BOOT_DIR}"
+
 cp -rp /boot/* "${BOOT_DIR}"
-cp -rp "${RECIPE_DIR}/files/boot/" "${BOOT_DIR}"
+cp -rp "${RECIPE_DIR}/files/boot/"* "${BOOT_DIR}"
--- bakery/repositories/core/recipes/rpi-debian-setup/steps/01-install.sh
+++ bakery/repositories/core/recipes/rpi-debian-setup/steps/01-install.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-BOOT_DIR="${RUGPI_BUNDLE_DIR}/roots/boot"
+BOOT_DIR="${RUGPI_BUNDLE_DIR}/roots/boot/"
 
 install -m 644 "${RECIPE_DIR}/files/raspberrypi.list" "/etc/apt/sources.list.d/"
 sed -i "s/RELEASE/bookworm/g" "/etc/apt/sources.list.d/raspberrypi.list"
@@ -22,4 +22,4 @@ install -m 644 "${RECIPE_DIR}/files/cmdline.txt" "/boot/firmware/"
 install -m 644 "${RECIPE_DIR}/files/config.txt" "/boot/firmware/"
 
 mkdir -p "${BOOT_DIR}"
-cp -rp /boot/firmware/ "${BOOT_DIR}"
+cp -rp /boot/firmware/* "${BOOT_DIR}"
