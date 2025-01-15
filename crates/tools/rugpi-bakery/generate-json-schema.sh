5566b6510721eb56fb488930b0501971e4903474
Maximilian Köhl <mail@koehlma.de>
2025-01-15 08:01:03 +0100

    fix relative paths

--- crates/tools/rugpi-bakery/generate-json-schema.sh
+++ crates/tools/rugpi-bakery/generate-json-schema.sh
@@ -4,9 +4,9 @@ mkdir -p generated
 rm -rf generated/*
 sidex generate json-schema generated/
 
-mkdir -p ../../schemas
-cp generated/rugix_bakery.projects.ProjectConfig.schema.json ../../schemas/rugix-bakery-project.schema.json
-cp generated/rugix_bakery.layers.LayerConfig.schema.json ../../schemas/rugix-bakery-layer.schema.json
-cp generated/rugix_bakery.recipes.RecipeConfig.schema.json ../../schemas/rugix-bakery-recipe.schema.json
-cp generated/rugix_bakery.tests.TestConfig.schema.json ../../schemas/rugix-bakery-test.schema.json
-cp generated/rugix_bakery.repositories.RepositoryConfig.schema.json ../../schemas/rugix-bakery-repository.schema.json
+mkdir -p ../../../schemas
+cp generated/rugix_bakery.projects.ProjectConfig.schema.json ../../../schemas/rugix-bakery-project.schema.json
+cp generated/rugix_bakery.layers.LayerConfig.schema.json ../../../schemas/rugix-bakery-layer.schema.json
+cp generated/rugix_bakery.recipes.RecipeConfig.schema.json ../../../schemas/rugix-bakery-recipe.schema.json
+cp generated/rugix_bakery.tests.TestConfig.schema.json ../../../schemas/rugix-bakery-test.schema.json
+cp generated/rugix_bakery.repositories.RepositoryConfig.schema.json ../../../schemas/rugix-bakery-repository.schema.json
